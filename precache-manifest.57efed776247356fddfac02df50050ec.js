30b3c786ebef0ef013c7ec1599a3eb222332b92a
reubenwedson <reubenwedson@gmail.com>
2020-10-25 23:01:42 +0300

    Updates

---
+++ precache-manifest.57efed776247356fddfac02df50050ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d29521e05e8d8d3342ec5de471be362",
+    "url": "/personalportfolio/index.html"
+  },
+  {
+    "revision": "1e86f8796055d567b265",
+    "url": "/personalportfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "989ebaa290caff02536a",
+    "url": "/personalportfolio/static/js/2.ca01c2d3.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/personalportfolio/static/js/2.ca01c2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e86f8796055d567b265",
+    "url": "/personalportfolio/static/js/main.ea5b8f2e.chunk.js"
+  },
+  {
+    "revision": "a6bede04cf10fd475013",
+    "url": "/personalportfolio/static/js/runtime-main.d67c2a1d.js"
+  }
+]);
